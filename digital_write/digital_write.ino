9abce78b4dc652676cde749013ac7ba0d66e3499
Deep Majumder <deep.majumder2019@gmail.com>
2024-01-01 19:57:05 +0530

    Utilizing fake QEMU sleep address to do delays

--- digital_write/digital_write.ino
+++ digital_write/digital_write.ino
@@ -1,17 +1,28 @@
+#ifdef delay
+#undef delay
+#endif
+
+#define delay qemu_delay
+#define QEMU_DELAY_ADDR 0x40800000
+
+void qemu_delay(uint32_t time_ms) {
+	volatile uint32_t *ptr = (volatile uint32_t *)QEMU_DELAY_ADDR;
+	*ptr = time_ms;
+}
 
 void setup() {
 	pinMode(5, OUTPUT);
 }
 
-void fixed_delay() {
-	for (int i = 0; i < (1 << 30); i++) {
-		asm volatile ("nop" : : : "memory");
-	}
-}
+// void fixed_delay() {
+// 	for (int i = 0; i < (1 << 30); i++) {
+// 		asm volatile ("nop" : : : "memory");
+// 	}
+// }
 
 void loop() {
 	digitalWrite(5, 0);
-	fixed_delay();
+	delay(500);
 	digitalWrite(5, 1);
-	fixed_delay();
+	delay(500);
 }
